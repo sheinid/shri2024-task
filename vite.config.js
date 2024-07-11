6b4bcfdd5c8d4891a04b07ecf31c6dcdc5870c26
Sheinid <sheinid@ya.ru>
2024-07-12 00:02:41 +0400

    fix: update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
-import MillionLint from '@million/lint';
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import MillionLint from "@million/lint";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 const _plugins = [react()];
-_plugins.unshift(MillionLint.vite())
+_plugins.unshift(MillionLint.vite());
 export default defineConfig({
-  plugins: _plugins
+  plugins: _plugins,
+  base: "/shri2024-task/",
 });
